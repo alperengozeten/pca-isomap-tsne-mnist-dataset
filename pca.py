d1a0b6cd296b686e1555d7e47a569a019ac6a895
HP <alperengozeten@gmail.com>
2023-05-01 13:32:05 +0300

    stack data to from train and test

--- pca.py
+++ pca.py
@@ -19,12 +19,26 @@ print(y.shape)
 # concatenate the data
 data = np.concatenate((x, y), axis=1)
 print(data)
-print(data.shape)
 
+test_data = []
+train_data = []
 for i in range(0, 10):
     class_i = data[data[:, 0] == i]
     np.random.shuffle(class_i)
-    
+
     mid = (len(class_i) // 2)
     test_i = class_i[0:mid, :]
     train_i = class_i[mid: , :]
+
+    test_data.append(test_i)
+    train_data.append(train_i)
+
+# concatenate and shuffle the data to form the test data
+test_data = np.concatenate(test_data, axis=0)
+np.random.shuffle(test_data)
+print(test_data.shape)
+
+# concatenate and shuffle the data to form the train data
+train_data = np.concatenate(train_data, axis=0)
+np.random.shuffle(train_data)
+print(train_data.shape)
