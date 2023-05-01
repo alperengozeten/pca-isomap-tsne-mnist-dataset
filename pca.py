ebeb8b95bed1fc07b14f9adcba3c02f367c9addc
HP <alperengozeten@gmail.com>
2023-05-01 13:22:21 +0300

    randomly select each class

--- pca.py
+++ pca.py
@@ -20,3 +20,11 @@ print(y.shape)
 data = np.concatenate((x, y), axis=1)
 print(data)
 print(data.shape)
+
+for i in range(0, 10):
+    class_i = data[data[:, 0] == i]
+    np.random.shuffle(class_i)
+    
+    mid = (len(class_i) // 2)
+    test_i = class_i[0:mid, :]
+    train_i = class_i[mid: , :]
